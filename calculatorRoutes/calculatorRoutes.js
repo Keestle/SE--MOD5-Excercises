11a32f3e36b61369a9ffc051eb473adfc8fd182d
Keestle <yeeern98@gmail.com>
2024-01-23 14:18:21 +0800

    Updates to file with additional files

--- calculatorRoutes/calculatorRoutes.js
+++ calculatorRoutes/calculatorRoutes.js
@@ -1,46 +1,26 @@
 const express = require('express');
+const calculatorController = require('../controllers/calculatorControllers')
 const router = express.Router();
+
 
 // Addition
 router.get('/add', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 + number2;
-    console.log(add);
-    res.status(200).json({ result: add });
+    calculatorController.addNumbers(req,res)
 });
 
 // Subtraction
 router.get('/sub', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sub = number1 - number2;
-    console.log(sub);
-    res.status(200).json({ result: sub });
+    calculatorController.subNumbers(req,res)
 });
 
 // Multiplication
 router.get('/multiply', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let product = number1 * number2;
-    console.log(product);
-    res.status(200).json({ result: product });
+    calculatorController.multiplyNumbers(req,res)
 });
 
 // Division
 router.get('/divide', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    
-    if (number2 === 0) {
-        res.status(400).json({ error: 'Cannot divide by zero' });
-        return;
-    }
-
-    let quotient = number1 / number2;
-    console.log(quotient);
-    res.status(200).json({ result: quotient });
+        calculatorController.divideNumbers(req,res)
 });
 
 module.exports = router;
